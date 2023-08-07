0d533a6257bc934fac6ff7d2ada7cbb40ad6a2b7
Mayinuddin Munna <mayinuddin.munna@gmail.com>
2023-08-07 19:55:20 +0600

    loading spinner added

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
+import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./pages/Router/Routes.jsx";
 
---
+++ src/pages/Shared/LoadingSpinner/LoadingSpinner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoadingSpinner = () => {
+    return (
+        <div>
+            <h1>Loading Spinner</h1>
+        </div>
+    );
+};
+
+export default LoadingSpinner;
