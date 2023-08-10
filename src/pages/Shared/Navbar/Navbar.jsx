b283748d8a526f367acc291193e73e617f0cbabb
Mayinuddin Munna <mayinuddin.munna@gmail.com>
2023-08-10 23:32:23 +0600

    some line indentation

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,8 +1,9 @@
+import "./Navbar.css";
 import React, { useState } from "react";
-import Logo from "../../../assets/Logo.png"
+import Logo from "../../../assets/Logo.png";
 import { Link } from "react-router-dom";
 import { FaBars, FaXmark } from "react-icons/fa6";
-import './Navbar.css'
+
 const Navbar = () => {
   const [isNavOpen, setIsNavOpen] = useState(true);
 
@@ -10,13 +11,17 @@ const Navbar = () => {
     setIsNavOpen(!isNavOpen);
   };
 
-  const navItem = <>
-    <Link className="text-red" to="/">Home</Link>
-    <Link to="/features">Feature</Link>
-    <Link to="/community">Community</Link>
-    <Link to="/new-metting">New Metting</Link>
-    <Link to="/login">Login</Link>
-  </>
+  const navItem = (
+    <>
+      <Link className="text-red" to="/">
+        Home
+      </Link>
+      <Link to="/features">Feature</Link>
+      <Link to="/community">Community</Link>
+      <Link to="/new-metting">New Metting</Link>
+      <Link to="/login">Login</Link>
+    </>
+  );
 
   return (
     <div className="container mx-auto py-2 flex items-center justify-between">
@@ -25,17 +30,12 @@ const Navbar = () => {
       </div>
       <div>
         <button onClick={handleNavToggle} className="md:hidden text-3xl">
-          {
-            isNavOpen ? <FaBars></FaBars>  : <FaXmark></FaXmark>
-          }
-
+          {isNavOpen ? <FaBars></FaBars> : <FaXmark></FaXmark>}
         </button>
         <ul className="hidden md:block">
-          <li className="nav-items font-semibold text-lg">
-            {navItem}
-          </li>
+          <li className="nav-items font-semibold text-lg">{navItem}</li>
         </ul>
-        <ul className={`md:hidden ${isNavOpen ? 'hidden' : 'static'}`}>
+        <ul className={`md:hidden ${isNavOpen ? "hidden" : "static"}`}>
           <li className="nav-items-responsive font-semibold text-lg">
             {navItem}
           </li>
