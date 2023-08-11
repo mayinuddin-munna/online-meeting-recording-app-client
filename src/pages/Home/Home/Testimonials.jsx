28e9d32afaebd8b0a7b1a78bddb9175665b21b3e
Ulfath Ara Islam <mouliajsahid@gmail.com>
2023-08-11 09:16:21 +0900

    testimonial component is designed

--- src/pages/Home/Home/Testimonials.jsx
+++ src/pages/Home/Home/Testimonials.jsx
@@ -1,11 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+import { FaQuoteLeft } from 'react-icons/fa';
+
+
+
 
 const Testimonials = () => {
-    return (
-        <div>
-            <h2>Testimonials</h2>
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    fetch('review.json')
+      .then(res => res.json())
+      .then(data => setReviews(data));
+  }, []);
+
+  return (
+    <>
+      {/* title */}
+      <div className="md:w-4/12 mx-auto text-center my-8">
+        <p className="text-red-600 mb-2">TESTIMONIALS</p>
+        <h3 className="text-xl uppercase py-4">What People Say About Our Services</h3>
+      </div>
+
+      <div className="py-10 ">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+
+          <Swiper
+            slidesPerView={3}
+            spaceBetween={20}
+            pagination={{ clickable: true }}
+            modules={[Pagination]}
+
+          >
+            {reviews.map(review => (
+              <SwiperSlide key={review._id}>
+                <div className="bg-white rounded-lg shadow p-6 border sm:flex lg:flex-col h-[400px]">
+                  <div className="relative overflow-y-auto">
+                    <img src={review.img} alt="Review" className="mx-auto w-24 h-24 rounded-full mb-7  sticky" />
+                    <div className="pl-4">
+                      <span className="flex items-center justify-center">
+                        <FaQuoteLeft className="text-red-500 text-[35px] mb-2" />
+                      </span>
+
+                      <p className="text-gray-700 mb-4">{review.details}</p>
+                      <h3 className="text-lg font-semibold">{review.name}</h3>
+                      <p className="text-gray-700 mb-4">{review.position}</p>
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+            ))}
+
+
+
+
+          </Swiper>
         </div>
-    );
+      </div>
+    </>
+
+  );
 };
 
 export default Testimonials;
