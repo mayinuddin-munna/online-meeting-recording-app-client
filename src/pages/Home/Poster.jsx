8fa520a78344b0e0b181f28d9b40fc229dc1162b
asad2002web <asad2002web@gmail.com>
2023-08-17 17:42:59 +0600

    fixed

--- src/pages/Home/Poster.jsx
+++ src/pages/Home/Poster.jsx
@@ -3,8 +3,8 @@ import ButtonArrow from '../../components/ButtonArrow';
 const Poster = () => {
   return (
     <div className='container mx-auto'>
-        <div className='flex flex-col md:flex-row justify-between items-center lg:w-2/3 mx-auto border-2 bg-slate-400 py-8 px-8 rounded-md bg-gradient-to-r from-purple-500 to-red-500'>
-            <h2 className='text-center md:text-left text-white font-semibold text-2xl'>Ensure your Business <br />
+        <div className='flex flex-col md:flex-row justify-between items-center lg:w-full mx-auto border-2 bg-slate-400 py-12 md:py-20 px-8 md:px-12 rounded-md bg-gradient-to-r from-purple-500 to-red-500'>
+            <h2 className='text-center md:text-left text-white font-semibold text-2xl md:text-3xl lg:text-5xl'>Ensure your Business <br />
             Continuty and Service Level
             </h2>
             <ButtonArrow>
