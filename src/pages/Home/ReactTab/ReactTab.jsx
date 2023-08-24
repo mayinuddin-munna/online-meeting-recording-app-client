a47b6a0fa36ac7d08be068839b238971f4115920
Ulfath Ara Islam <mouliajsahid@gmail.com>
2023-08-24 14:45:45 +0900

    solutions.json is added

--- src/pages/Home/ReactTab/ReactTab.jsx
+++ src/pages/Home/ReactTab/ReactTab.jsx
@@ -15,8 +15,8 @@ const YourComponent = () => {
                     {/* Content for Tab 1 */}
                     <div className="flex justify-center items-center flex-col-reverse md:flex-row lg:flex-row mb-5 ">
                         <div className="ms-5">
-                            <h1 className="text-5xl font-bold mb-4 md:text-7xl lg:text-7xl text-center">Connect with calling</h1>
-                            <p className="text-left leading-9 text-2xl text-slate-600 ms-5 me-5 hidden md:hidden lg:block">
+                            <h1 className="text-5xl font-bold mb-4 md:text-3xl lg:text-5xl text-center">Connect with calling</h1>
+                            <p className="text-left leading-9 text-2xl md:text-xl text-slate-600 ms-5 me-5   lg:block">
                                 With our advanced calling technology, you can foster seamless communication across borders, time zones, and platforms. Whether it's team meetings, client presentations, or international conferences, our calling solution ensures crystal-clear audio and smooth video connections. Say goodbye to geographical limitations and hello to productive and efficient interactions.
                             </p>
 
@@ -28,8 +28,8 @@ const YourComponent = () => {
                     {/* Content for Tab 2 */}
                     <div className="flex justify-center items-center flex-col-reverse md:flex-row lg:flex-row mt-8 mb-5 ">
                         <div className="ms-5">
-                            <h1 className="text-5xl font-bold mb-3 md:text-7xl lg:text-7xl text-center">Meet from anywhere</h1>
-                            <p className="text-left leading-9 text-2xl text-slate-600 ms-5 me-5 hidden md:hidden lg:block">
+                            <h1 className="text-5xl font-bold mb-4 md:text-3xl lg:text-5xl text-center">Meet from anywhere</h1>
+                            <p className="text-left leading-9 text-2xl text-slate-600 ms-5 me-5 ">
                                 Connect and collaborate seamlessly, no matter where you are. Our cutting-edge video conferencing technology empowers you to hold productive meetings and discussions from the comfort of your own space. Whether you're at the office, at home, or on the go, our platform ensures that you're always just a click away from meaningful interactions.
                             </p>
 
