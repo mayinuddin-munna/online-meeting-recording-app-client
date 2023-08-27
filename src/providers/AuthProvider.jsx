124918282c83c71945f5b51af516a0563f0d685e
Doha0 <abu.jardoha@gmail.com>
2023-08-27 20:14:23 +0600

    register provider created

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,23 +1,61 @@
-import { GoogleAuthProvider, getAuth } from 'firebase/auth';
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, updateProfile } from 'firebase/auth';
 import React from 'react';
 import { createContext } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import app from '../../firebase.config';
+import { useEffect } from 'react';
+import { loginUser, setLoading } from '../features/userSlice';
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
 
+    const dispatch = useDispatch();
+
     const user = useSelector((state) => state.data.user.user);
     const isLoading = useSelector((state) => state.data.user.isLoading);
 
     const googleProvider = new GoogleAuthProvider();
 
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const updateUserProfile = (username) => {
+        return updateProfile(auth.currentUser, {
+            displayName: username
+        });
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (authUser) => {
+            if (authUser) {
+                dispatch(
+                    loginUser({
+                        uid: authUser.uid,
+                        username: authUser.displayName,
+                        email: authUser.email,
+                    })
+                );
+                dispatch(setLoading(false));
+            } else {
+                dispatch(setLoading(false));
+                // console.log("User is not logged in.");
+            }
+        });
+        return () => {
+            return unsubscribe();
+        }
+    }, [dispatch]);
+
 
     const authInfo = {
         user,
         isLoading,
+        createUser,
+        updateUserProfile
     }
 
     return (
