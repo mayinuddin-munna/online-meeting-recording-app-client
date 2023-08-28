59fdf195338bd448e31a60fa2da74cd3d4ba2ea6
Ulfath Ara Islam <mouliajsahid@gmail.com>
2023-08-28 18:07:10 +0900

    footer is updated

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,43 +1,41 @@
 import React from "react";
-import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
-import Logo from "../../../assets/Logo.png";
 import { Link } from "react-router-dom";
+import { Typewriter } from 'react-simple-typewriter'
+
 const Footer = () => {
+
   return (
-    <footer className="bg-[#6EE7B7]  py-12">
-      <div className="container mx-auto flex flex-col md:flex-row items-center justify-between">
-        <div className="w-16 mb-6 md:mb-0">
-          <img src={Logo} alt="" />
+    <footer className="bg-gradient-to-r from-green-400 to-blue-400 py-12">
+      <div className="max-w-2xl mx-auto py-10">
+        <div className="text-center">
+        <div className="text-4xl mb-5">
+        <Typewriter
+            words={['Connect with Galaxy Meet']}
+            loop={true}
+            cursor
+            cursorStyle='_'
+            typeSpeed={60}
+            deleteSpeed={50}
+            delaySpeed={1000}
+          />
         </div>
-        <div className="space-y-4 md:space-y-0 md:space-x-6">
-          <a href="#" className="hover:text-emerald-900 transition-colors">
-            Home
-          </a>
-          <a href="#" className="hover:text-emerald-900 transition-colors">
-            About Us
-          </a>
-          <a href="#" className="hover:text-emerald-900 transition-colors">
-            Feature
-          </a>
-          {/* contact page linked */}
-          <Link to='/contact' className="hover:text-emerald-900 transition-colors">Contact</Link>
+          <p>Stay connected. Anytime, anywhere.</p>
+          <div className="flex justify-center my-10">
+            <a href="#" className="flex items-center border w-auto rounded-lg px-4 py-2 mx-2 hover:bg-gray-100 transition-colors">
+              <span className="text-xl">Join the Meeting</span>
+            </a>
+          </div>
         </div>
-        <div className="flex space-x-4">
-          <a href="#" className=" hover:text-emerald-900 transition-transform transform hover:scale-110">
-            <FaFacebook className="text-2xl"/>
-          </a>
-          <a href="#" className=" hover:text-emerald-900 transition-transform transform hover:scale-110">
-            <FaTwitter className="text-2xl"/>
-          </a>
-          <a href="#" className=" hover:text-emerald-900 transition-transform transform hover:scale-110">
-            <FaInstagram className="text-2xl"/>
-          </a>
+        <div className="mt-28 flex flex-col md:flex-row md:justify-between items-center text-sm">
+          <p className="order-2 md:order-1 mt-8 md:mt-0">
+            &copy; 2023 Galaxy Meeting. All Rights Reserved.
+          </p>
+          <div className="order-1 md:order-2">
+            <Link><span className="px-2">About us</span></Link>
+            <Link><span className="px-2 border-l">Contact us</span></Link>
+            <Link><span className="px-2 border-l">Privacy Policy</span></Link>
+          </div>
         </div>
-      </div>
-      <div className="mt-8 text-center">
-        <p className="text-gray-500 text-sm">
-          © 2023 Galaxy Meeting. All Rights Reserved.
-        </p>
       </div>
     </footer>
   );
