6fd2e51100e272dd8c6b3afd872fd607d4ded032
Ulfath Ara Islam <mouliajsahid@gmail.com>
2023-08-28 23:34:37 +0900

    footer section is updated

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,40 +1,37 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Typewriter } from 'react-simple-typewriter'
+import { BsInfoCircle } from "react-icons/bs";
+import { FaEnvelope, FaFacebook } from "react-icons/fa";
+import Logo from "../../../assets/Logo.png";
 
 const Footer = () => {
-
   return (
-    <footer className=" bg-gradient-to-r from-green-400 to-blue-400 py-12">
-      <div className="max-w-2xl mx-auto py-10">
-        <div className="text-center">
-        <div className="text-4xl mb-5">
-        <Typewriter
-            words={['Connect with Galaxy Meet']}
-            loop={true}
-            cursor
-            cursorStyle='_'
-            typeSpeed={60}
-            deleteSpeed={50}
-            delaySpeed={1000}
-          />
+    <footer class="w-full py-2 flex items-center justify-center bg-gradient-to-r from-emerald-300 via-emerald-100 to-emerald-200">
+      <div class="md:w-2/3 w-full px-4 text-white flex flex-col ">
+
+        <div class="flex sm:items-center sm: mt-8 justify-center">
+          <img src={Logo} className="w-[100px] hidden md:block text-center" alt="" />
         </div>
-          <p>Stay connected. Anytime, anywhere.</p>
-          <div className="flex justify-center my-10">
-            <a href="#" className="flex items-center border w-auto rounded-lg px-4 py-2 mx-2 hover:bg-gray-100 transition-colors">
-              <span className="text-xl">Join the Meeting</span>
-            </a>
+        <div class="flex flex-col">
+          <div class="flex mt-24 mb-12 flex-row justify-center md:justify-between">
+            <a class="hidden md:block cursor-pointer text-gray-600 hover:text-green-700 uppercase">About</a>
+            <a class="hidden md:block cursor-pointer text-gray-600 hover:text-green-700 uppercase">Services</a>
+            <a class="hidden md:block cursor-pointer text-gray-600 hover:text-green-700 uppercase">Why us</a>
+            <a class="hidden md:block cursor-pointer text-gray-600 hover:text-green-700 uppercase">Contact</a>
+            <div class="flex flex-row space-x-4 items-center justify-between">
+              <a className="text-4xl hover:text-green-700 ">
+              <BsInfoCircle />
+              </a>
+              <a className="text-4xl hover:text-green-700 ">
+              <FaEnvelope />
+              </a>
+              <a className="text-4xl hover:text-green-700 ">
+              <FaFacebook />
+              </a>
+              
+            </div>
           </div>
-        </div>
-        <div className="mt-28 flex flex-col md:flex-row md:justify-between items-center text-sm">
-          <p className="order-2 md:order-1 mt-8 md:mt-0">
-            &copy; 2023 Galaxy Meeting. All Rights Reserved.
-          </p>
-          <div className="order-1 md:order-2">
-            <Link><span className="px-2">About us</span></Link>
-            <Link><span className="px-2 border-l">Contact us</span></Link>
-            <Link><span className="px-2 border-l">Privacy Policy</span></Link>
-          </div>
+          <hr class="border-gray-600" />
+          <p class="w-full text-center my-12 text-gray-600">© 2023 Galaxy Meeting. All Rights Reserved.</p>
         </div>
       </div>
     </footer>
