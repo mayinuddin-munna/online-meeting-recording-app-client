c7d23f4a13dbe5b477f44c712c32151faa05e699
Ulfath Ara Islam <mouliajsahid@gmail.com>
2023-08-30 18:40:50 +0900

    react tab is updated

--- src/pages/Home/ReactTab/ReactTab.jsx
+++ src/pages/Home/ReactTab/ReactTab.jsx
@@ -3,59 +3,55 @@ import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 
 const ReactTab = () => {
-    const [tab, setTab] = useState([]);
-    const [activeTab, setActiveTab] = useState(0);
+  const [tab, setTab] = useState([]);
+  const [activeTab, setActiveTab] = useState(0);
 
-    useEffect(() => {
-        fetch("tabs.json")
-            .then((res) => res.json())
-            .then((data) => {
-                console.log(data);
-                setTab(data);
-            })
-    }, []);
+  useEffect(() => {
+    fetch("tabs.json")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setTab(data);
+      });
+  }, []);
 
-    return (
-        <div className=" mt-20 container mx-auto mb-10">
-            <Tabs>
+  return (
+    <div className=" mt-20 container mx-auto mb-10 h-[70vh]">
+      <Tabs>
+        <TabList className="text-center text-xl border-b-2 border-transparent flex space-x-4 overflow-x-auto mb-10">
+          {tab.map((item) => (
+            <Tab
+              key={item._id}
+              onClick={() => setActiveTab(item._id)}
+              className={`px-3 py-1 md:w-1/4 lg:w-1/5 uppercase cursor-pointer ${
+                activeTab === item._id ? "tab-border" : ""
+              } hover:text-[#5EC38B]`}
+            >
+              {item.name}
+            </Tab>
+          ))}
+        </TabList>
 
-                {/* Todo: border green is not working */}
-                <TabList className='text-center text-2xl border-b-2 border-transparent flex space-x-4 mb-10'>
-                    {tab.map((item) => (
-                        <Tab
-                            key={item._id}
-                            onClick={() => setActiveTab(item._id)}
-                            className={`w-1/2 md:w-1/4 lg:w-1/5 ${activeTab === item._id ? "border-b-2 border-green-500 outline-none" : ""
-                                }`}
-                        >
-                            {item.name}
-                        </Tab>
-                    ))}
-                </TabList>
-
-
-
-                {tab.map((item) => (
-                    <TabPanel key={item._id}>
-                        {/* Content for each tab */}
-                        <div className="flex justify-center items-center flex-col-reverse md:flex-row lg:flex-row mb-5 ">
-                            <div className="ms-5">
-                                <h1 className="text-5xl font-bold mb-4 md:text-3xl lg:text-5xl text-center">
-                                    {item.title}
-                                </h1>
-                                <p className="text-left leading-9 text-2xl md:text-xl text-slate-600 ms-5 me-5 lg:block">
-                                    {item.content}
-                                </p>
-                            </div>
-                            <img className="w-[400px] h-[400]" src={item.img} alt="" />
-                        </div>
-                    </TabPanel>
-                ))}
-
-            </Tabs>
-        </div>
-
-    );
+        {tab.map((item) => (
+          <TabPanel key={item._id}>
+            <div className="flex justify-center items-center flex-col-reverse md:flex-row lg:flex-row mb-5">
+              <div className="ms-5 me-5 md:w-1/2">
+                <h1 className="text-5xl font-bold mt-5 mb-4 md:text-3xl lg:text-5xl text-center">
+                  {item.title}
+                </h1>
+                <p className="text-left leading-9 text-2xl md:text-xl text-slate-600 ms-5 me-5 lg:block">
+                  {item.content}
+                </p>
+              </div>
+              <div className="w-1/2">
+                <img className="" src={item.img} alt="" />
+              </div>
+            </div>
+          </TabPanel>
+        ))}
+      </Tabs>
+    </div>
+  );
 };
 
 export default ReactTab;
