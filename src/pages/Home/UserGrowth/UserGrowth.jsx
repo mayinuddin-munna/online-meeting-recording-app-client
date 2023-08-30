a7046f66cb95e0100edbb542fb50846c9d756afc
Doha0 <abu.jardoha@gmail.com>
2023-08-30 11:45:10 +0600

    user growth section added

--- src/pages/Home/UserGrowth/UserGrowth.jsx
+++ src/pages/Home/UserGrowth/UserGrowth.jsx
@@ -1,26 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import "./UserGrowth.css"
 
 const UserGrowth = () => {
     return (
-        <section className="my-10 lg:my-36">
-            <div className="container grid grid-cols-1 lg:grid-cols-2 justify-center lg:justify-between p-6 mx-auto sm:py-12 lg:py-24 ">
+        <section className="my-16 lg:my-36">
+            <div className="container grid grid-cols-1 lg:grid-cols-2 gap-x-10 justify-center lg:justify-between p-6 mx-auto sm:py-12 lg:py-24 ">
 
-
-                <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
-                    <h1 className="text-5xl font-bold sm:text-6xl">
-                        Trusted By Users Everywhere
+                <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:text-left">
+                    <h1 className="text-4xl lg:text-6xl font-bold ">
+                        Loved By Users Everywhere
                     </h1>
-                    <p className="mt-6 mb-8 text-lg sm:mb-12">
-
+                    <p className="my-8 text-base lg:text-lg sm:mb-12">
+                        92% of users expressing their love for the Galaxy Meeting application,
+                        its no wonder our application is experiencing rapid growth. The simplicity, enjoyment,
+                        and security our application offers during meetings are getting positive response.
                     </p>
                     <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
-                        <a rel="noopener noreferrer" href="#" className="px-8 py-3 text-lg font-semibold rounded dark:bg-violet-400 dark:text-gray-900">Join Us Now</a>
-                        <a rel="noopener noreferrer" href="#" className="px-8 py-3 text-lg font-semibold border rounded dark:border-gray-100">Login</a>
+                        <Link to="/register" className="user-growth-btn1">Join Us Now</Link>
+                        <Link to="/login" className="user-growth-btn2">Login Now</Link>
                     </div>
                 </div>
 
-                <div className="flex items-center justify-center p-6 mt-8 lg:mt-0">
-                    <img src="https://i.ibb.co/N2wTm98/Galaxy-Meet-Banner.png" alt="" className="object-cover" />
+                <div className="flex items-center justify-center p-0 lg:p-6 mt-8 mb-12 lg:mb-0 lg:mt-0">
+                    <img src="https://i.ibb.co/N2wTm98/Galaxy-Meet-Banner.png" alt="user growth img" className="object-cover" />
                 </div>
             </div>
         </section>
