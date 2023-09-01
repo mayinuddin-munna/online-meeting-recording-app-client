caf498dab2a18685c07c555468352e49511d75cc
Doha0 <abu.jardoha@gmail.com>
2023-09-01 18:22:59 +0600

    new poster

--- src/pages/Home/Poster.jsx
+++ src/pages/Home/Poster.jsx
@@ -7,8 +7,8 @@ const Poster = () => {
     <div className="container mx-auto">
       <div className="flex flex-col md:flex-row justify-between items-center lg:w-10/12 mx-auto border-2 bg-slate-400 
       py-10 px-12 rounded-md  bg-gradient-to-r from-blue-400 via-cyan-400 to-teal-300">
-        <h2 className="text-center md:text-left text-white font-semibold text-2xl">
-          Ensure your Business Continuity and Service Level
+        <h2 className="text-center md:text-left font-semibold text-2xl  lg:text-3xl text-white">
+          Ensure your Business Continuity and Service with Galaxy Meet
         </h2>
         <ButtonArrow>
           <Link>
