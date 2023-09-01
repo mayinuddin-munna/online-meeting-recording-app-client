d13d7ea5f46051cf84d62614977291000208930d
Ulfath Ara Islam <mouliajsahid@gmail.com>
2023-09-01 22:01:37 +0900

    footer is responsive

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -7,8 +7,8 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white  pb-5">
-      <div className="container mx-auto py-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        <div className="flex flex-col items-center">
+      <div className="container md:text-center mx-auto py-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="flex flex-col items-center ">
           <div className="mb-4">
             <img src={Logo} alt="Logo" className="w-17 h-16" />
           </div>
@@ -30,19 +30,19 @@ const Footer = () => {
             </Link>
           </div>
         </div>
-        <div className="space-y-4">
+        <div className="space-y-4 md:ms-2 lg:ms-32 ">
           <p className="font-semibold">Our Team</p>
           <p>FAQ</p>
           <p>Contact</p>
           <p>Reviews</p>
         </div>
-        <div className="flex flex-col space-y-4">
-          <p className="font-semibold">Subscribe to connect us</p>
-          <div className="flex space-x-2">
+        <div className="flex flex-col md:text-center md:ms-5 md:mt-3 space-y-4 ">
+          <p className="font-semibold">Subscribe to connect with us</p>
+          <div className="flex">
             <input
               type="email"
               placeholder="Your Email"
-              className="px-2 py-1 border text-gray-500 border-gray-400 rounded-l outline-none"
+              className="lg:px-2 border text-gray-500 border-gray-400 rounded-l outline-none"
             />
             <button className="p-3 w-[110px] rounded rounded-l-none hover:bg-[#5EC38B] hover:text-white border shadow border-l-0">
               Subscribe
@@ -50,6 +50,7 @@ const Footer = () => {
           </div>
         </div>
       </div>
+
       <hr className="mt-8 border-t border-gray-600" />
       <div className="text-center pt-2 text-sm mb-0">
         <p>
