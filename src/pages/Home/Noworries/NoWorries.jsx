3ffc0330d8ef16d3af270de5fe5b03b3d733a545
asad2002web <asad2002web@gmail.com>
2023-09-04 18:32:14 +0600

    worries fix

--- src/pages/Home/Noworries/NoWorries.jsx
+++ src/pages/Home/Noworries/NoWorries.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { RiDeviceLine, RiSignalWifiLine  } from "react-icons/ri";
+import { RiDeviceLine, RiSignalWifiLine, RiVideoChatFill  } from "react-icons/ri";
 import { RoughNotation } from "react-rough-notation";
+import DoNotWorry from "../../../assets/do-not-worry.png"
 const NoWorries = () => {
   return (
-    <div className="h-[100vh]">
-      <div className=" bg-[#111827] h-[40vh] flex items-center text-center">
+    <div>
+      <div className=" bg-[#111827] py-10 md:py-0 md:h-[50vh] flex items-center text-center">
         <div className="container mx-auto text-white md:w-1/2">
           <h1 className="text-3xl font-semibold mb-4">
           <RoughNotation type="underline" show={true} color="yellow">No Worries, No Risk</RoughNotation>
@@ -16,11 +17,11 @@ const NoWorries = () => {
           </p>
         </div>
       </div>
-      <div className=" bg-[#050C1B] h-[60vh] flex items-center">
+      <div className=" bg-[#050C1B] py-12 md:h-[80vh] flex items-center">
         <div className="container mx-auto text-white flex items-center flex-col md:flex-row gap-8 md:gap-4">
           <div className="flex-1">
             <p className="text-center text-2xl">
-              What can I expect <br />during the test?
+              <img className="w-56 block mx-auto" src={DoNotWorry} alt="" />
             </p>
           </div>
           <div className="flex-1">
@@ -30,7 +31,7 @@ const NoWorries = () => {
               </div>
               <div className="text">
                 <h2 className="text-xl font-semibold mb-2">
-                  <RoughNotation type="underline" show={true} color="yellow">Seamless Multi-Device Integration</RoughNotation>
+                 Seamless Multi-Device Integration
                   </h2>
                 <p>Easily switch between devices without interruption during video calls. Start a call on your computer, then seamlessly transition to your mobile device without disconnecting, ensuring you stay connected no matter where you are.</p>
               </div>
@@ -41,10 +42,23 @@ const NoWorries = () => {
               </div>
               <div className="text">
                 <h2 className="text-xl font-semibold mb-2">
-                <RoughNotation type="underline" show={true} color="yellow">Adaptive Bandwidth Optimization</RoughNotation>
+               Adaptive Bandwidth Optimization
                   </h2>
                 <p>
                 GalaxyMeet intelligently adjusts video quality based on your network connection, providing a smooth video calling experience even in low-bandwidth environments. Enjoy clear communication without disruptions, regardless of your internet speed.
+                </p>
+              </div>
+            </div>
+            <div className="flex items-start gap-6 mt-4">
+              <div className="icon-worries text-2xl bg-[#31D4D2] px-3 rounded-lg py-3 text-black">
+                <RiVideoChatFill  />
+              </div>
+              <div className="text">
+                <h2 className="text-xl font-semibold mb-2">
+                Highly Secure Video Calls
+                  </h2>
+                <p>
+                Our video calling solution is built to meet strict regulatory standards. We implement robust security protocols and encryption to ensure your video calls are both secure and compliant with relevant regulations. Rest easy knowing your sensitive conversations are protected and legally sound.
                 </p>
               </div>
             </div>
