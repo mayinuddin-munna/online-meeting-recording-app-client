33ba9f79469881500907dea08d4cf36dcb2b162a
Doha0 <abu.jardoha@gmail.com>
2023-09-15 10:59:08 +0600

    meeting quality data added

--- src/pages/Layout/Dashboard/Pages/MeetingData/MeetingData.jsx
+++ src/pages/Layout/Dashboard/Pages/MeetingData/MeetingData.jsx
@@ -7,6 +7,8 @@ const MeetingData = () => {
         <div className='py-16 lg:py-20'>
             <h2 className='text-5xl text-center font-semibold text-white'>Meeting Analytics</h2>
             <MeetingQuality></MeetingQuality>
+            
+
         </div>
     );
 };
